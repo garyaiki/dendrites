b725019255547b538d8376eed683b6b7f88eff50
Gary Struthers <agilej2ee@earthlink.net>
2014-12-02 17:44:09 -0800

    field, ring

--- src/main/scala/org/gs/algebird/package.scala
+++ src/main/scala/org/gs/algebird/package.scala
@@ -2,7 +2,7 @@
   */
 package org.gs
 
-import com.twitter.algebird.{ Group, Monoid, Semigroup }
+import com.twitter.algebird.{ Field, Group, Monoid, NumericRing, Ring, Semigroup }
 
 /** @author garystruthers
   *
@@ -10,7 +10,7 @@ import com.twitter.algebird.{ Group, Monoid, Semigroup }
 package object algebird {
 
   /** Sums list elements
-    * @tparam A element type
+    * @tparam A element type that can be added
     * @param xs list
     * @param ev Semigroup for type A
     * @return Option of sum or None for empty list
@@ -18,7 +18,7 @@ package object algebird {
   def sumOption[A](xs: List[A])(implicit ev: Semigroup[A]): Option[A] = ev.sumOption(xs)
 
   /** Sums list elements
-    * @tparam A element type
+    * @tparam A element type that can be added and has a zero identity
     * @param xs list
     * @param ev Monoid for type A
     * @return sum or Monoid[A] zero for empty list
@@ -26,26 +26,50 @@ package object algebird {
   def sum[A](xs: List[A])(implicit ev: Monoid[A]): A = ev.sum(xs)
 
   /** Negates an element
-    * @tparam A element type
-    * @param xs list
+    * @tparam A element type that can be negated
+    * @param x element to negate
     * @param ev Group for type A
-    * @return sum or Monoid[A] zero for empty list
+    * @return -x
     */
   def negate[A](x: A)(implicit ev: Group[A]): A = ev.negate(x)
 
-  implicit object BigDecimalSemigroup extends Semigroup[BigDecimal] {
-    def plus(a: BigDecimal, b: BigDecimal) = a + b
+  /** Subtracts an element from another
+    * @tparam A element type that can be negated
+    * @param l
+    * @param r
+    * @param ev Group for type A
+    * @return l - r
+    */
+  def minus[A](l: A, r: A)(implicit ev: Group[A]): A = ev.minus(l, r)
+
+  /** Multiplies an element by another
+    * @tparam A element type that can be multiplied
+    * @param l
+    * @param r
+    * @param ev Ring for type A
+    * @return l * r
+    */
+  def times[A](l: A, r: A)(implicit ev: Ring[A]): A = ev.times(l, r)
+
+  /** Multiplies list elements
+    * @tparam A element type that can be multiplied and has a one identity
+    * @param xs list
+    * @param ev Ring for type A
+    * @return sum or Monoid[A] zero for empty list
+    */
+  def product[A](xs: List[A])(implicit ev: Ring[A]): A = ev.product(xs)
+
+  /** Reciprocal of an element
+    * @tparam A element type that can divide 1 and return an A
+    * @param x element to divide one
+    * @param ev Field for type A
+    * @return 1/x
+    */
+  def inverse[A](x: A)(implicit ev: Field[A]): A = ev.inverse(x)
+
+  implicit object BigDecimalField extends NumericRing[BigDecimal] with Field[BigDecimal] {
+    override def inverse(v: BigDecimal): BigDecimal = 1/ v
   }
 
-  implicit object BigDecimalMonoid extends Monoid[BigDecimal] {
-    def zero = BigDecimal(0)
-    def plus(a: BigDecimal, b: BigDecimal) = BigDecimalSemigroup.plus(a, b)
-  }
-
-  implicit object BigDecimalGroup extends Group[BigDecimal] {
-    def zero = BigDecimalMonoid.zero
-    def plus(a: BigDecimal, b: BigDecimal) = BigDecimalMonoid.plus(a, b)
-    override def negate(v: BigDecimal): BigDecimal = minus(zero, v) 
-  }
 
 }
