4eef1005e022dfcf2e8ca9f68bbfb02a4c9612f2
Gary Struthers <agilej2ee@earthlink.net>
2014-12-20 19:47:54 -0800

    Option, Either case classes

--- src/test/scala/org/gs/CaseClassLike.scala
+++ src/test/scala/org/gs/CaseClassLike.scala
@@ -11,6 +11,24 @@ case class KeyInt(k: String, v: Int)
 case class KeyLong(k: String, v: Long)
 case class KeyString(k: String, v: String)
 
+case class KeyOptBigDec(k: String, v: Option[BigDecimal])
+case class KeyOptBigInt(k: String, v: Option[BigInt])
+case class KeyOptBool(k: String, v: Option[Boolean])
+case class KeyOptDouble(k: String, v: Option[Double])
+case class KeyOptFloat(k: String, v: Option[Float])
+case class KeyOptInt(k: String, v: Option[Int])
+case class KeyOptLong(k: String, v: Option[Long])
+case class KeyOptStr(k: String, v: Option[String])
+
+case class KeyEithBigDec(k: String, v: Either[String, BigDecimal])
+case class KeyEithBigInt(k: String, v: Either[String, BigInt])
+case class KeyEithBool(k: String, v: Either[String, Boolean])
+case class KeyEithDouble(k: String, v: Either[String, Double])
+case class KeyEithFloat(k: String, v: Either[String, Float])
+case class KeyEithInt(k: String, v: Either[String, Int])
+case class KeyEithLong(k: String, v: Either[String, Long])
+case class KeyEithStr(k: String, v: Either[String, String])
+
 import annotation.implicitNotFound
 @implicitNotFound(msg = "Cannot find CaseClassLike type class for ${A}")
 trait CaseClassLike[A, B] {
@@ -20,6 +38,13 @@ trait CaseClassLike[A, B] {
   *
   */
 object CaseClassLike {
+  def altTF(k: Char) = if (k % 2 == 0) (true) else (false)
+  implicit val mc = new java.math.MathContext(2)
+  def exDouble(k: Char)(implicit ev: java.math.MathContext) = BigDecimal(k.toInt * 1.1, ev).toDouble
+  def exFloat(k: Char) = k.toInt * 1.1f
+  def exStr(k: Char) = k.toString.toUpperCase()
+  def exLeftStr(k: Char) = s"Error key:$k"
+
   implicit object CaseClassLikeBigDecimal extends CaseClassLike[BigDecimal, KeyBigDecimal] {
     def apply(k: Char): KeyBigDecimal = KeyBigDecimal(k.toString, k.toInt)
   }
@@ -27,13 +52,13 @@ object CaseClassLike {
     def apply(k: Char): KeyBigInt = KeyBigInt(k.toString, k.toInt)
   }
   implicit object CaseClassLikeBoolean extends CaseClassLike[Boolean, KeyBoolean] {
-    def apply(k: Char): KeyBoolean = KeyBoolean(k.toString, if(k % 2 == 0) true else false)
+    def apply(k: Char): KeyBoolean = KeyBoolean(k.toString, altTF(k))
   }
   implicit object CaseClassLikeDouble extends CaseClassLike[Double, KeyDouble] {
-    def apply(k: Char): KeyDouble = KeyDouble(k.toString, k.toInt * 1.1)
+    def apply(k: Char): KeyDouble = KeyDouble(k.toString, exDouble(k))
   }
   implicit object CaseClassLikeFloat extends CaseClassLike[Float, KeyFloat] {
-    def apply(k: Char): KeyFloat = KeyFloat(k.toString, k.toInt * 1.1f)
+    def apply(k: Char): KeyFloat = KeyFloat(k.toString, exFloat(k))
   }
   implicit object CaseClassLikeInt extends CaseClassLike[Int, KeyInt] {
     def apply(k: Char): KeyInt = KeyInt(k.toString, k.toInt)
@@ -42,6 +67,63 @@ object CaseClassLike {
     def apply(k: Char): KeyLong = KeyLong(k.toString, k.toInt)
   }
   implicit object CaseClassLikeString extends CaseClassLike[String, KeyString] {
-    def apply(k: Char): KeyString = KeyString(k.toString, k.toString.toUpperCase())
+    def apply(k: Char): KeyString = KeyString(k.toString, exStr(k))
   }
+
+  implicit object CaseClassLikeOptBigDec extends CaseClassLike[Option[BigDecimal], KeyOptBigDec] {
+    def apply(k: Char): KeyOptBigDec = KeyOptBigDec(k.toString, Some(k.toInt))
+  }
+  implicit object CaseClassLikeOptBigInt extends CaseClassLike[Option[BigInt], KeyOptBigInt] {
+    def apply(k: Char): KeyOptBigInt = KeyOptBigInt(k.toString, Some(k.toInt))
+  }
+  implicit object CaseClassLikeOptBool extends CaseClassLike[Option[Boolean], KeyOptBool] {
+    def apply(k: Char): KeyOptBool = KeyOptBool(k.toString, Some(altTF(k)))
+  }
+  implicit object CaseClassLikeOptDouble extends CaseClassLike[Option[Double], KeyOptDouble] {
+    def apply(k: Char): KeyOptDouble = KeyOptDouble(k.toString, Some(exDouble(k)))
+  }
+  implicit object CaseClassLikeOptFloat extends CaseClassLike[Option[Float], KeyOptFloat] {
+    def apply(k: Char): KeyOptFloat = KeyOptFloat(k.toString, Some(exFloat(k)))
+  }
+  implicit object CaseClassLikeOptInt extends CaseClassLike[Option[Int], KeyOptInt] {
+    def apply(k: Char): KeyOptInt = KeyOptInt(k.toString, Some(k.toInt))
+  }
+  implicit object CaseClassLikeOptLong extends CaseClassLike[Option[Long], KeyOptLong] {
+    def apply(k: Char): KeyOptLong = KeyOptLong(k.toString, Some(k.toInt))
+  }
+  implicit object CaseClassLikeOptStr extends CaseClassLike[Option[String], KeyOptStr] {
+    def apply(k: Char): KeyOptStr = KeyOptStr(k.toString, Some(exStr(k)))
+  }
+
+  implicit object CaseClassLikeEithBigDec extends
+      CaseClassLike[Either[String, BigDecimal], KeyEithBigDec] {
+    def apply(k: Char): KeyEithBigDec = KeyEithBigDec(k.toString, Right(k.toInt))
+  }
+  implicit object CaseClassLikeEithBigInt extends
+      CaseClassLike[Either[String, BigInt], KeyEithBigInt] {
+    def apply(k: Char): KeyEithBigInt = KeyEithBigInt(k.toString, Right(k.toInt))
+  }
+  implicit object CaseClassLikeEithBool extends
+      CaseClassLike[Either[String, Boolean], KeyEithBool] {
+    def apply(k: Char): KeyEithBool = KeyEithBool(k.toString, Right(altTF(k)))
+  }
+  implicit object CaseClassLikeEithDouble extends
+      CaseClassLike[Either[String, Double], KeyEithDouble] {
+    def apply(k: Char): KeyEithDouble = KeyEithDouble(k.toString, Right(exDouble(k)))
+  }
+  implicit object CaseClassLikeEithFloat extends
+      CaseClassLike[Either[String, Float], KeyEithFloat] {
+    def apply(k: Char): KeyEithFloat = KeyEithFloat(k.toString, Right(exFloat(k)))
+  }
+  implicit object CaseClassLikeEithInt extends
+      CaseClassLike[Either[String, Int], KeyEithInt] {
+    def apply(k: Char): KeyEithInt = KeyEithInt(k.toString, Right(k.toInt))
+  }
+  implicit object CaseClassLikeEithLong extends CaseClassLike[Either[String, Long], KeyEithLong] {
+    def apply(k: Char): KeyEithLong = KeyEithLong(k.toString, Right(k.toInt))
+  }
+  implicit object CaseClassLikeEithStr extends CaseClassLike[Either[String, String], KeyEithStr] {
+    def apply(k: Char): KeyEithStr = KeyEithStr(k.toString, Right(exStr(k)))
+  }
+
 }
--- src/test/scala/org/gs/TestValuesBuilder.scala
+++ src/test/scala/org/gs/TestValuesBuilder.scala
@@ -9,11 +9,10 @@ import scala.collection.immutable.NumericRange
   *
   */
 trait TestValuesBuilder {
-  
-  
-  def makeCaseClasses[A, B](keyRange: NumericRange.Inclusive[Char])(implicit ev: CaseClassLike[A, B]
-    ): Seq[B] = {
-    for(k <- keyRange) yield {
+
+  def makeCaseClasses[A, B](keyRange: NumericRange.Inclusive[Char])(
+    implicit ev: CaseClassLike[A, B]): Seq[B] = {
+    for (k <- keyRange) yield {
       ev.apply(k)
     }
   }
@@ -26,6 +25,25 @@ trait TestValuesBuilder {
   val keyInt = makeCaseClasses[Int, KeyInt](keyRange)
   val keyLong = makeCaseClasses[Long, KeyLong](keyRange)
   val keyString = makeCaseClasses[String, KeyString](keyRange)
+
+  val keyOptBigDec = makeCaseClasses[Option[BigDecimal], KeyOptBigDec](keyRange)
+  val keyOptBigInt = makeCaseClasses[Option[BigInt], KeyOptBigInt](keyRange)
+  val keyOptBool = makeCaseClasses[Option[Boolean], KeyOptBool](keyRange)
+  val keyOptDouble = makeCaseClasses[Option[Double], KeyOptDouble](keyRange)
+  val keyOptFloat = makeCaseClasses[Option[Float], KeyOptFloat](keyRange)
+  val keyOptInt = makeCaseClasses[Option[Int], KeyOptInt](keyRange)
+  val keyOptLong = makeCaseClasses[Option[Long], KeyOptLong](keyRange)
+  val keyOptStr = makeCaseClasses[Option[String], KeyOptStr](keyRange)
+
+  val keyEithBigDec = makeCaseClasses[Either[String, BigDecimal], KeyEithBigDec](keyRange)
+  val keyEithBigInt = makeCaseClasses[Either[String, BigInt], KeyEithBigInt](keyRange)
+  val keyEithBool = makeCaseClasses[Either[String, Boolean], KeyEithBool](keyRange)
+  val keyEithDouble = makeCaseClasses[Either[String, Double], KeyEithDouble](keyRange)
+  val keyEithFloat = makeCaseClasses[Either[String, Float], KeyEithFloat](keyRange)
+  val keyEithInt = makeCaseClasses[Either[String, Int], KeyEithInt](keyRange)
+  val keyEithLong = makeCaseClasses[Either[String, Long], KeyEithLong](keyRange)
+  val keyEithStr = makeCaseClasses[Either[String, String], KeyEithStr](keyRange)
+  
   val bigDecimals = extractElement[BigDecimal](keyBigDecimal, 1)
   val bigInts = extractElement[BigInt](keyBigInt, 1)
   val doubles = extractElement[Double](keyDouble, 1)
@@ -34,4 +52,19 @@ trait TestValuesBuilder {
   val longs = extractElement[Long](keyLong, 1)
   val strings = extractElement[String](keyString, 1)
 
+  val optBigDecs = extractElement[Option[BigDecimal]](keyOptBigDec, 1)
+  val optBigInts = extractElement[Option[BigInt]](keyOptBigInt, 1)
+  val optDoubles = extractElement[Option[Double]](keyOptDouble, 1)
+  val optFloats = extractElement[Option[Float]](keyOptFloat, 1)
+  val optInts = extractElement[Option[Int]](keyOptInt, 1)
+  val optLongs = extractElement[Option[Long]](keyOptLong, 1)
+  val optStrs = extractElement[Option[String]](keyOptStr, 1)
+
+  val eithBigDecs = extractElement[Either[String, BigDecimal]](keyEithBigDec, 1)
+  val eithBigInts = extractElement[Either[String, BigInt]](keyEithBigInt, 1)
+  val eithDoubles = extractElement[Either[String, Double]](keyEithDouble, 1)
+  val eithFloats = extractElement[Either[String, Float]](keyEithFloat, 1)
+  val eithInts = extractElement[Either[String, Int]](keyEithInt, 1)
+  val eithLongs = extractElement[Either[String, Long]](keyEithLong, 1)
+  val eithStrs = extractElement[Either[String, String]](keyEithStr, 1)
 }
