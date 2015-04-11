9743e5b7909474c62d96ad2b27df232ef022cb89
Gary Struthers <agilej2ee@earthlink.net>
2015-04-10 18:06:45 -0700

    Aggregator apply, select methods

--- src/main/scala/org/gs/aggregator/package.scala
+++ src/main/scala/org/gs/aggregator/package.scala
@@ -5,6 +5,7 @@ package org.gs
 /** @author garystruthers
   *
   */
+import scala.annotation.tailrec
 package object aggregator {
   import shapeless._
   import syntax._
@@ -16,10 +17,32 @@ package object aggregator {
       val hl2 = hl.toZipper.insert(x).reify
       Aggregate[HList](hl2)
     }
-    
+
     def hasAll(total: Int): Boolean = {
       val count = new HListOps(hl).runtimeLength
       if (count < total) false else true
+    }
+
+    def apply(n: Int): Product = {
+      @tailrec
+      def loop(l: HList, acc: Int): Product = l match {
+        case hd :: tl if (acc == n) => hd match { case a: Product => a }
+        case hd :: tl               => loop(tl, acc + 1)
+        case _ => None
+      }
+
+      loop(hl, 0)
+    }
+
+    def select(x: Product): Product = {
+      @tailrec
+      def loop(l: HList): Product = l match {
+        case hd :: tl if(hd.getClass == x.getClass) => hd match { case a: Product => a }
+        case hd :: tl => loop(tl)
+        case _ => None
+      }
+
+      loop(hl)
     }
   }
 }
