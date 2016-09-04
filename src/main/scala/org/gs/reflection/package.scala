db320038f1c61942379c5184a192d4bc184d1ac3
Gary Struthers <agilej2ee@earthlink.net>
2016-09-03 17:33:43 -0700

    comments

--- src/main/scala/org/gs/reflection/package.scala
+++ src/main/scala/org/gs/reflection/package.scala
@@ -1,4 +1,17 @@
+/** Copyright 2016 Gary Struthers
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.gs
 
 import scala.reflect.runtime.universe.{Type, TypeRef, WeakTypeTag}
@@ -15,15 +28,16 @@ import scala.reflect.runtime.universe.{Type, TypeRef, WeakTypeTag}
   */
 package object reflection {
 
-  /** Report type information, uses scala's enhanced reflection, useful for logging and debugging
-    *
+  /** Report type information, can be references to type parameters or abstract types. Use for
+    * logging and debugging
     * to use, import scala.reflect.runtime.universe._
     * Warning Not thread safe, should be fine for logging and debugging though
+    *
+    * @tparam T type to get info for
+    * @param tag implicit WeakTypeTag[T]
+    * @return type information of element
     * @see [[http://docs.scala-lang.org/overviews/reflection/thread-safety.html reflection thread-safety]]
     * @see [[http://docs.scala-lang.org/overviews/reflection/overview.html reflection overview]]
-    *
-    * @param implicit WeakTypeTag
-    * @return type information of element
     */
   def weakParamInfo[T](x: T)(implicit tag: WeakTypeTag[T]): List[Type] =
     tag.tpe match { case TypeRef(_, _, args) => args }
